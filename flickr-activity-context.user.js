ef93942594d7e1035835efade576f9c7e182a2c4
Joel Courtney <euphemize@gmail.com>
2010-08-18 23:08:36 +1000

    Update to context - all contexts included now

--- flickr-activity-context.user.js
+++ flickr-activity-context.user.js
@@ -1,20 +1,28 @@
 // ==UserScript==
 // @name          Flickr Activity Context Switcher
 // @namespace     http://github.com/jufemaiz/jmc_flickr_gm_flickr-activity-context
-// @description   Removes the activity stream context from activity pages (returns browsing of images to user only context)
-// @include       http://*flickr.com/
-// @include		  http://*flickr.com/photos/friends/
+// @description   Removes the context browsing from flickr (returns browsing of images to user only context). Note: this breaks your use of lightroom for that context!
+// @include       http://*flickr.com*
 //
 // ==/UserScript==
 //
 
-var main = document.getElementById('Main');
-var elements = main.getElementsByTagName('a');
+// Options - use either true or false.
+var options = {
+	contacts : true,
+	sets : false,
+	groups : false,
+	favorites : false
+};
 
+// Do not edit after here!
+var main = document.getElementById('Main') || document.getElementById('main');
+var elements = main.getElementsByTagName('a');
 for(var i = 0; i < elements.length; i++) {
     var el = elements[i];
-    if(el.href.indexOf("/in/contacts/") != -1) {
-		el.href = el.href.replace("/in/contacts/","");
-	}
-	
+	el.href = el.href.replace(/\/in\/photostream\//,"");
+    if(el.href.indexOf("/in/contacts/") != -1 && options.contacts) { el.href = el.href.replace(/\/in\/contacts\//,""); }
+    if(el.href.indexOf("/in/set-") != -1 && options.sets) { el.href = el.href.replace(/\/in\/set-\d+\//,""); }
+    if(el.href.indexOf("/in/pool-") != -1 && options.sets) { el.href = el.href.replace(/\/in\/pool-[A-Za-z@\d]+\//,""); }
+    if(el.href.indexOf("/in/faves-") != -1 && options.sets) { el.href = el.href.replace(/\/in\/faves-[A-Za-z@\d]+\//,""); }
 }
